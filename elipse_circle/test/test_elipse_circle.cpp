35e005f3a1fafd82a3ae7bf62a35cd8290a61ee7
Han Zitong <hanzitong1218@gmail.com>
2024-06-27 10:31:30 +0900

    micro

--- elipse_circle/test/test_elipse_circle.cpp
+++ elipse_circle/test/test_elipse_circle.cpp
@@ -12,11 +12,11 @@ TEST(IntersectionTest, RangeSolution) {
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double> dis_a(30, 60);
     double a = dis_a(gen);
-    std::uniform_real_distribution<double> dis_bdiff(5, 15);
+    std::uniform_real_distribution<double> dis_bdiff(5, 20);
     double b = a - dis_bdiff(gen);
-    std::uniform_real_distribution<double> dis_c(a + 10, 60);
+    std::uniform_real_distribution<double> dis_c(a + 10, 2 * a - 10);
     double c = dis_c(gen);
-    std::uniform_real_distribution<double> dis_r(c, c + a);
+    std::uniform_real_distribution<double> dis_r(c - a + 10, c + a);
     double r = dis_r(gen);
 
     /* skiving */
@@ -28,11 +28,11 @@ TEST(IntersectionTest, RangeSolution) {
     // std::uniform_real_distribution<double> dis_r(100, 225);
     // double r = dis_r(gen);
 
-    /* must condition */
-    assert(r <= c + a);     // if false, elipse is in circle. no intersection.
-    assert(r + a >= c);     // if false, elipse is out of circle. no intersection.
+    /* check if test condition is correct */
+    ASSERT_LE(r, c + a);    // if false, elipse is in circle. no intersection.
+    ASSERT_GE(r + a, c);    // if false, elipse is out of circle. no intersection.
 
-    // calculate intersection
+    /* calculate intersection of elipse and circle */
     elipse_circle::FuncConstant(a, b, c, r);
     double x = elipse_circle::calc_intersection(a, b, c, r);
 
@@ -43,7 +43,7 @@ TEST(IntersectionTest, RangeSolution) {
     /* test if the intersection calculated from circle is on the elipse */
     // calculate y of solution with circle function
     double ysq = r*r - std::pow(x + c, 2.);
-    ASSERT_GE(ysq, 0.);     // true value condition
+    ASSERT_GE(ysq, 0.);     // test true value condition
     double y_upper = std::sqrt(ysq);  
     double y_lower = -1. * y_upper; 
 
@@ -55,7 +55,6 @@ TEST(IntersectionTest, RangeSolution) {
     double elipse_lower = (x * x) / (a * a) + (y_lower * y_lower) / (b * b);
     ASSERT_NEAR(elipse_lower, 1., 0.001);
 }
-
 
 
 int main(int argc, char **argv){
